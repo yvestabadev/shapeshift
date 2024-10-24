054a2f4513d08fbe6278b5f339ba74411f27c0b7
Yves Taba <117912315+nossbank@users.noreply.github.com>
2024-10-24 15:05:49 -0300

    isolando a peça menor e definindo via sistema a posição dela

--- src/main/java/br/com/yvestaba/Tabuleiro.java
+++ src/main/java/br/com/yvestaba/Tabuleiro.java
@@ -27,6 +27,37 @@ public class Tabuleiro implements Cloneable{
         this.matriz[y][x] = (this.matriz[y][x] + 1) % qtdeTipoPeca;
     }
 
+    public void jogarPecaDefinirCoordenada(MapeadorDePosicao mapeador){
+        for(int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz[0].length; j++){
+                if(matriz[i][j] != 0){
+                    boolean[][] peca = mapeador.getPeca().get();
+                    Coordenada coordenadaAtual = definirCoordenada(peca, j, i);
+                    mapeador.setCoordenadaAtual(coordenadaAtual);
+                    try {
+                        jogarPeca(peca, coordenadaAtual);
+                    } catch (Exception e){
+                        jogarPeca(peca, new Coordenada(0, 0));
+                    }
+                    return;
+                }
+            }
+        }
+    }
+
+    //define de acordo com a primeira coordenada da peça preenchida
+    //ex: [[false, true][true, true]] a primeira preenchida é [0][1], então acrescenta 1 à coordenada x
+    private Coordenada definirCoordenada(boolean[][] peca, int x, int y) {
+        int largura = x;
+        for(int i = 0; i < peca[0].length; i++){
+            if(peca[0][i]){
+                break;
+            }
+            largura--;
+        }
+        return new Coordenada(largura, y);
+    }
+
     //a peça do objetivo tem valor zero
     public boolean ganhou(){
         return Arrays.deepEquals(new int[matriz.length][matriz[0].length], matriz);
