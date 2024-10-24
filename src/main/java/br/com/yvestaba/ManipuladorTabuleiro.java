8f58128ff57fbed3a7e2db843f4737f04bd21146
Yves Taba <117912315+nossbank@users.noreply.github.com>
2024-10-24 16:21:45 -0300

    limpando a classe tabuleiro e atribuindo a definição de coordenada da última peça a outra classe

--- src/main/java/br/com/yvestaba/ManipuladorTabuleiro.java
+++ src/main/java/br/com/yvestaba/ManipuladorTabuleiro.java
@@ -8,6 +8,33 @@ public class ManipuladorTabuleiro {
         for(var mapeador : mapeadoresOrganizados){
             tabuleiro.jogarPeca(mapeador.getPeca().get(), mapeador.getCoordenadaAtual());
         }
-        tabuleiro.jogarPecaDefinirCoordenada(ultimoMapeamento);
+        for(int y = 0; y < tabuleiro.getTamanhoY(); y++){
+            for(int x = 0; x < tabuleiro.getTamanhoX(); x++){
+                if(tabuleiro.getPecaCoordenada(y, x) != 0){
+                    var peca = ultimoMapeamento.getPeca().get();
+                    Coordenada coordenadaAtual = definirCoordenada(peca, x, y);
+                    ultimoMapeamento.setCoordenadaAtual(coordenadaAtual);
+                    try {
+                        tabuleiro.jogarPeca(peca, coordenadaAtual);
+                    } catch (Exception e){
+                        tabuleiro.jogarPeca(peca, new Coordenada(0, 0));
+                    }
+                    return;
+                }
+            }
+        }
+    }
+
+    //define de acordo com a primeira coordenada da peça preenchida
+    //ex: [[false, true][true, true]] a primeira preenchida é [0][1], então acrescenta 1 à coordenada x
+    private static Coordenada definirCoordenada(boolean[][] peca, int x, int y) {
+        int largura = x;
+        for(int i = 0; i < peca[0].length; i++){
+            if(peca[0][i]){
+                break;
+            }
+            largura--;
+        }
+        return new Coordenada(largura, y);
     }
 }
